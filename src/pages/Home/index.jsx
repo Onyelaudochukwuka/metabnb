f659ed2c2334a069b738c28c280334969a8c01ac
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 19:30:07 +0100

    added component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import { Hero, Navbar, Places, Sponsors } from '../../components';
+import { Cta, Hero, Navbar, Places, Sponsors } from '../../components';
 
 import style from './index.module.scss';
 
 const Home = () => (
-    <section className={style.Home}>
-      <Navbar />
-      <Hero />
-      <Sponsors />
-      <Places />
-    </section>
+  <section className={style.Home}>
+    <Navbar />
+    <Hero />
+    <Sponsors />
+    <Places />
+    <Cta />
+  </section>
 );
 
 export default Home;
