6f68560c5f323b419c608452c3be84e155a4078b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 21:10:09 +0100

    fixes

--- src/components/molecules/PlaceToStayContent/index.jsx
+++ src/components/molecules/PlaceToStayContent/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Filter } from '../../../assets';
+
 import style from './index.module.scss';
 
 const filters = [
@@ -15,10 +17,14 @@ const filters = [
 const PlaceToStayContent = () => {
   return (
     <section className={style.PlaceToStayContent}>
-      <div>
+      <div className={style.PlaceToStayContent__filters}>
         {filters.map((filter) => (
           <button key={filter} type="button">{filter}</button>
         ))}
+        <div className={style.PlaceToStayContent__filters__icon}>
+          <span>Location</span>
+          <Filter />
+        </div>
       </div>
     </section>
   );
