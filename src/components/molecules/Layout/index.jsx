6e37b43fb24c70e30c5b54bf14af28c041275017
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 22:20:23 +0100

    added logic

--- src/components/molecules/Layout/index.jsx
+++ src/components/molecules/Layout/index.jsx
@@ -5,13 +5,11 @@ import { Navbar, Footer, PopUp } from '../..';
 import style from './index.module.scss';
 // eslint-disable-next-line
 const Layout = ({ children, ...props }) => {
-  const [popUp, setPopUp] = useState(false);
+  const [popUp, setPopUp] = useState(true);
   return (
-    <section
-      {...props}
-      className={style.Layout}>
-      <Navbar openPopUp={() => setPopUp(true)} />
-      <PopUp {...{ popUp, toggle: () => setPopUp(props => !props) }} />
+    <section {...props} className={style.Layout}>
+      <Navbar openPopUp={() => setPopUp((props) => !props)} />
+      <PopUp {...{ popUp, toggle: () => setPopUp((props) => !props) }} />
       {children}
       <Footer />
     </section>
