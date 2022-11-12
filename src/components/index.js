6ef5fdccf9e020dae26eaf03ec9f774021242bef
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 20:29:48 +0100

    exports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from './molecules/Hero';
 export { default as Sponsors } from './molecules/Sponsors';
 export { default as Places } from './molecules/Places';
 export { default as Cta } from './molecules/Cta';
+export { default as Footer } from './molecules/Footer';
