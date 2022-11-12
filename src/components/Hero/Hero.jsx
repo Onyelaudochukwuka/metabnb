048b586e7960cda327c9c89daa6b3d7cf2748acf
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 12:17:40 +0100

    created hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Hero = () => {
+  return (
+    <section className={style.Hero}>
+      <div>Hero</div>
+    </section>
+  );
+};
+
+// mastery by robert hilm
+export default Hero;
