ebcca95280189c270d7679c421852699f6f3d2ab
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 19:28:50 +0100

    exported components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './molecules/Navbar';
 export { default as Hero } from './molecules/Hero';
 export { default as Sponsors } from './molecules/Sponsors';
 export { default as Places } from './molecules/Places';
+export { default as Cta } from './molecules/Cta';
