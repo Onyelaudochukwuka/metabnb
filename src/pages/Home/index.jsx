1685e5919724b2b4db59405f6be9ae9c10a71745
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 16:50:22 +0100

    added hero section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Navbar } from '../../components';
+import { Hero, Navbar } from '../../components';
 import style from './index.module.scss';
 // eslint-disable-next-line
 const Home = () => {
   return (
     <section className={style.Home}>
       <Navbar />
+      <Hero />
     </section>
   );
 };
