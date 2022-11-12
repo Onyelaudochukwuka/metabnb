b5b1c99b6793d2ea9e580d37f848264adb4861bd
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:13:00 +0100

    created sponsors component

---
+++ src/components/molecules/Sponsors/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Sponsors = () => {
+  return (
+    <section className={style.Sponsors}>
+      <div>Sponsors</div>
+    </section>
+  );
+};
+
+export default Sponsors;
