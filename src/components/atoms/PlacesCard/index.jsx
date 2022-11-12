9664c2f150be5c38c12d57f7d25541bde0301edd
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:54:03 +0100

    adding props

--- src/components/atoms/PlacesCard/index.jsx
+++ src/components/atoms/PlacesCard/index.jsx
@@ -1,25 +1,63 @@
 import React, { useId } from 'react';
 
+import PropTypes from 'prop-types';
+
 import { Like, Star } from '../../../assets';
 
 import style from './index.module.scss';
 
-const PlacesCard = () => {
+const PlacesCard = ({
+  image,
+  title,
+  payment,
+  distance,
+  availability,
+  stars,
+}) => {
   const id = useId();
-  const stars = Array(5).fill(null);
-  const nullStars = Array(5 - 5).fill(null);
+  const starsArr = Array(stars).fill(null);
+  const nullStars = Array(5 - stars).fill(null);
   return (
     <section className={style.PlacesCard}>
-      <img src="/hero1.png" alt='places card' />
+      <img src="/hero1.png" alt="places card" />
       <Like className={style.PlacesCard__like} />
-      <div className={style.PlacesCard__description}><small>Desert king</small><small><b>1MBT per night</b></small></div>
-      <div className={style.PlacesCard__description}><small>2345km away</small><small>available for 2weeks stay</small></div>
+      <div className={style.PlacesCard__description}>
+        <small>Desert king</small>
+        <small>
+          <b>1MBT per night</b>
+        </small>
+      </div>
+      <div className={style.PlacesCard__description}>
+        <small>2345km away</small>
+        <small>available for 2weeks stay</small>
+      </div>
       <div className={style.PlacesCard__stars}>
-        {stars.map((_, i) => (<Star key={id + i} />))}
-        {nullStars.map((_, i) => (<Star key={id + i} fill="#002279" />))}
+        {starsArr.map((_, i) => (
+          <Star key={id + i} />
+        ))}
+        {nullStars.map((_, i) => (
+          <Star key={id + i} fill="#002279" />
+        ))}
       </div>
     </section>
   );
+};
+
+PlacesCard.defaultProps = {
+  image: 'places1',
+  title: 'Desert king',
+  payment: '1MBT per night',
+  distance: '2345km away',
+  availability: 'available for 2weeks stay',
+  stars: 5,
+};
+PlacesCard.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  payment: PropTypes.string,
+  distance: PropTypes.string,
+  availability: PropTypes.string,
+  stars: PropTypes.number,
 };
 
 export default PlacesCard;
