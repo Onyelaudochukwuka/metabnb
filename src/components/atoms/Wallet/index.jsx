911ff74903230fcb5fd850a2a96269175f16557c
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 21:46:27 +0100

    created wallet component

---
+++ src/components/atoms/Wallet/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Wallet = () => {
+  return (
+    <section className={style.Wallet}>
+      <div>Wallet</div>
+    </section>
+  );
+};
+
+export default Wallet;
