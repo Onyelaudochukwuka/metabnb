4481d104aa75f8976cdcd7b95f7cd62752be511b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:23:32 +0100

    added icons

--- src/components/molecules/Sponsors/index.jsx
+++ src/components/molecules/Sponsors/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { MBToken, Metamask, OpenSea } from '../../../assets';
 import style from './index.module.scss';
 // eslint-disable-next-line
 const Sponsors = () => {
   return (
     <section className={style.Sponsors}>
-      <div>Sponsors</div>
+      <MBToken />
+      <Metamask />
+      <OpenSea />
     </section>
   );
 };
