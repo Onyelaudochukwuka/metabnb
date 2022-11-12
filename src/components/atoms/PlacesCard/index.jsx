266e76d198361c7c23bacbd1fb9f2b8ee0ffc74b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:29:16 +0100

    created atom component

---
+++ src/components/atoms/PlacesCard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const PlacesCard = () => {
+  return (
+    <section className={style.PlacesCard}>
+      <div>PlacesCard</div>
+    </section>
+  );
+};
+
+export default PlacesCard;
