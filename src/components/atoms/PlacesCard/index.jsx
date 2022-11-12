84b9d651c17e64438f44e5bb3ef962db7e6ebc3b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:32:32 +0100

    added boilerplate code

--- src/components/atoms/PlacesCard/index.jsx
+++ src/components/atoms/PlacesCard/index.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useId } from 'react';
+
+import { Star } from '../../../assets';
 
 import style from './index.module.scss';
 
 const PlacesCard = () => {
+  const id = useId();
+  const stars = Array(5).fill(null);
+  const nullStars = Array(5 - 5).fill(null);
   return (
     <section className={style.PlacesCard}>
       <img src="/hero1.png" alt='places card' />
       <div className={style.PlacesCard__description}><small>Desert king</small><small><b>1MBT per night</b></small></div>
-      <div className={style.PlacesCard__description}><small>2345km away</small><small><b>available for 2weeks stay</b></small></div>
-
+      <div className={style.PlacesCard__description}><small>2345km away</small><small>available for 2weeks stay</small></div>
+      <div className={style.PlacesCard__stars}>
+        {stars.map((_, i) => (<Star key={id + i} />))}
+        {nullStars.map((_, i) => (<Star key={id + i} fill="#002279" />))}
+      </div>
     </section>
   );
 };
