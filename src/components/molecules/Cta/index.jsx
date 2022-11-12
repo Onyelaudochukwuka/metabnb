9cafb2bdfd808d08654a2b8569363e70752eff4c
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 19:21:21 +0100

    created component

---
+++ src/components/molecules/Cta/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Cta = () => {
+  return (
+    <section className={style.Cta}>
+      <div>Cta</div>
+    </section>
+  );
+};
+
+export default Cta;
