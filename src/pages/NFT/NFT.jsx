9a68f7e740ed4adb256fac11a8d1cc044a216542
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 12:19:27 +0100

    created nft page

---
+++ src/pages/NFT/NFT.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const NFT = () => {
+  return (
+    <section className={style.NFT}>
+      <div>NFT</div>
+    </section>
+  );
+};
+
+// mastery by robert hilm
+export default NFT;
