6c3da67e721d8d81b88fd571a9a64755bf8eb935
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:40:28 +0100

    exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,4 @@ export { default as MBToken } from './icons/MBToken';
 export { default as Metamask } from './icons/Metamask';
 export { default as OpenSea } from './icons/OpenSea';
 export { default as Star } from './icons/Star';
+export { default as Like } from './icons/Like';
