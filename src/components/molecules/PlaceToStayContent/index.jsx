449f7d058314624cafe4a1d79590542c2eb8cd0f
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 21:22:43 +0100

    fixes

--- src/components/molecules/PlaceToStayContent/index.jsx
+++ src/components/molecules/PlaceToStayContent/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Filter } from '../../../assets';
+import { PlacesCard } from '../../atoms';
 
 import style from './index.module.scss';
 
@@ -14,7 +15,136 @@ const filters = [
   'Off-grid',
   'Farm',
 ];
-
+const placesDetails = [
+  {
+    image: 'place-to-stay1',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay2',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay3',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay4',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay5',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay6',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay7',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay8',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay9',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay10',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay11',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay12',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay13',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay14',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay15',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'place-to-stay16',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+];
 const PlaceToStayContent = () => {
   return (
     <section className={style.PlaceToStayContent}>
@@ -26,6 +156,11 @@ const PlaceToStayContent = () => {
           <span>Location</span>
           <Filter />
         </div>
+      </div>
+      <div className={style.PlaceToStayContent__cards}>
+      {placesDetails.map((place) => (
+        <PlacesCard key={place.image} relativeUrl="place-to-stay" {...place} />
+      ))}
       </div>
     </section>
   );
