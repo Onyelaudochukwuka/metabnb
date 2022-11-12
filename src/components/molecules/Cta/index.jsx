d9e203db0137413c17c3fc6c5864144a2ef45982
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 20:14:05 +0100

    styling

--- src/components/molecules/Cta/index.jsx
+++ src/components/molecules/Cta/index.jsx
@@ -7,11 +7,19 @@ const Cta = () => {
     <section className={style.Cta}>
       <div className={style.Cta__container}>
         <h2>Metabnb NFTs</h2>
-        <p>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
-        <button type='button'>Learn more</button>
+        <p>
+          Discover our NFT gift cards collection. Loyal customers gets amazing
+          gift cards which are traded as NFTs. These NFTs gives our cutomer
+          access to loads of our exclusive services.
+        </p>
+        <button type="button">
+          <span>Learn more</span>
+        </button>
       </div>
-      <div>
-        
+      <div className={style.Cta__images}>
+        <img src="/cta/cta3.png" className={style.Cta__images__cta3} />
+        <img src="/cta/cta1.png" className={style.Cta__images__cta1} />
+        <img src="/cta/cta2.png" className={style.Cta__images__cta2} />
       </div>
     </section>
   );
