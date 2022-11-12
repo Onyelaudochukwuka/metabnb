6dbbc8f16c6c1efb918d57a85f26476b98bd0a7b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:54:05 +0100

    linting

--- src/components/molecules/Places/index.jsx
+++ src/components/molecules/Places/index.jsx
@@ -1,13 +1,82 @@
-import React from 'react';
+import React, { useId } from 'react';
 
 import { PlacesCard } from '../../atoms';
 
 import style from './index.module.scss';
-
+const placesDetails = [
+  {
+    image: 'places1',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places2',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places3',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places4',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places5',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places6',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places7',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+  {
+    image: 'places8',
+    title: 'Desert king',
+    payment: '1MBT per night',
+    distance: '2345km away',
+    availability: 'available for 2weeks stay',
+    stars: 5,
+  },
+];
 const Places = () => {
+  const id = useId();
   return (
     <section className={style.Places}>
       <h2>Inspiration for your next adventure</h2>
+      {placesDetails.map((place, i) => (
+        <PlacesCard key={id + i} {...place} />
+      ))}
       <PlacesCard />
     </section>
   );
