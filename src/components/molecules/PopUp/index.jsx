f1227b91eba8d387ec018ca072517b17b61d8ec5
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 21:46:29 +0100

    styling

--- src/components/molecules/PopUp/index.jsx
+++ src/components/molecules/PopUp/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
+import { Close } from '../../../assets';
+
 import style from './index.module.scss';
-// eslint-disable-next-line
+
 const PopUp = () => {
   return (
     <section className={style.PopUp}>
-      <div>
+      <div className={style.PopUp__container}>
         <header>
           <h3>Connect Wallet</h3>
-          
+          <Close />
         </header>
+        <div className={style.PopUp__container__wallets}>
+          <p>Choose your preferred wallet:</p>
+        </div>
       </div>
     </section>
   );
