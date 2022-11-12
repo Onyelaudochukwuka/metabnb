6d35b346d185de172e974eef7053a40f473ca742
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 20:59:48 +0100

    fixes

---
+++ src/components/molecules/PlaceToStayContent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './index.module.scss';
+
+const PlaceToStayContent = () => {
+  return (
+    <section className={style.PlaceToStayContent}>
+      <div>PlaceToStayContent</div>
+    </section>
+  );
+};
+
+export default PlaceToStayContent;
