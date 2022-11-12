76658e599da1f082997f24a55f1531a3e2b8afaa
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:10:39 +0100

    styling

--- src/components/molecules/Hero/index.jsx
+++ src/components/molecules/Hero/index.jsx
@@ -5,15 +5,15 @@ const images = ['hero2', 'hero1', 'hero4', 'hero3'];
 const Hero = () => {
   return (
     <section className={style.Hero}>
-      <div className={style.Hero__conatiner}>
-        <h1 className={style.Hero__conatiner__heading}>
+      <div className={style.Hero__container}>
+        <h1 className={style.Hero__container__heading}>
           Rent a <span>Place</span> away from <span>Home</span> in the{' '}
           <span>Metaverse</span>
         </h1>
         <p>we provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone</p>
-        <form>
-          <input type="text" placeholder="Search for location" />
-          <button type="submit">Search</button>
+        <form className={style.Hero__container__form}>
+          <input type="text" placeholder="Search for location" className={style.Hero__container__form__input } />
+          <button type="submit" className={style.Hero__container__form__submit}>Search</button>
         </form>
       </div>
       <div className={style.Hero__images}>
