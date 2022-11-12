c4252e6db79671133d69e52a975c81161f01fe13
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 20:15:23 +0100

    created footer component

---
+++ src/components/molecules/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Footer = () => {
+  return (
+    <section className={style.Footer}>
+      <div>Footer</div>
+    </section>
+  );
+};
+
+export default Footer;
