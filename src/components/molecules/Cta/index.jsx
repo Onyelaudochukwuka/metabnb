01106b08c857bc3c40d0256902c23b619ec59bd4
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 19:29:57 +0100

    addeed component

--- src/components/molecules/Cta/index.jsx
+++ src/components/molecules/Cta/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+
 import style from './index.module.scss';
-// eslint-disable-next-line
+
 const Cta = () => {
   return (
     <section className={style.Cta}>
-      <div>Cta</div>
+      <div className={style.Cta__container}>
+        <h2>Metabnb NFTs</h2>
+        <p>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
+        <button type='button'>Learn more</button>
+      </div>
+      <div>
+        
+      </div>
     </section>
   );
 };
