f878011dde311912ed4d1262dd0e55a2b69754ff
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:52:38 +0100

    added places card component

--- src/components/atoms/PlacesCard/index.jsx
+++ src/components/atoms/PlacesCard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+
 import style from './index.module.scss';
-// eslint-disable-next-line
+
 const PlacesCard = () => {
   return (
     <section className={style.PlacesCard}>
