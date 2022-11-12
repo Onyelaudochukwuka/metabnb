9aba9c762123e956d6aa38d52ca9334d5f4be9a5
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:32:05 +0100

    exported star

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,3 +2,4 @@ export { default as Logo } from './icons/Logo';
 export { default as MBToken } from './icons/MBToken';
 export { default as Metamask } from './icons/Metamask';
 export { default as OpenSea } from './icons/OpenSea';
+export { default as Star } from './icons/Star';
