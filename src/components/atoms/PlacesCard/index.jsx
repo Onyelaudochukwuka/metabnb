93fa4ec628e8b60d73f862ee0b8b42e283c02c4d
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 18:03:15 +0100

    added boilerplate code

--- src/components/atoms/PlacesCard/index.jsx
+++ src/components/atoms/PlacesCard/index.jsx
@@ -6,7 +6,9 @@ const PlacesCard = () => {
   return (
     <section className={style.PlacesCard}>
       <img src="/hero1.png" alt='places card' />
-      <div><span>PlacesCard</span></div>
+      <div className={style.PlacesCard__description}><small>Desert king</small><small><b>1MBT per night</b></small></div>
+      <div className={style.PlacesCard__description}><small>2345km away</small><small><b>available for 2weeks stay</b></small></div>
+
     </section>
   );
 };
