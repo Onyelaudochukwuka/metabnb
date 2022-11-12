ddc0a4f3e9c13b3e0b2a82c8c296997da6fb9748
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 20:58:34 +0100

    created PlaceTo stay component

---
+++ src/components/molecules/PlaceToStay/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const PlaceToStay = () => {
+  return (
+    <section className={style.PlaceToStay}>
+      <div>PlaceToStay</div>
+    </section>
+  );
+};
+
+export default PlaceToStay;
