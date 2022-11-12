cada13d4a0f7baf8abc91120f5d7288c8b05a9bf
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:29:47 +0100

    created places component

---
+++ src/components/molecules/Places/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Places = () => {
+  return (
+    <section className={style.Places}>
+      <div>Places</div>
+    </section>
+  );
+};
+
+export default Places;
