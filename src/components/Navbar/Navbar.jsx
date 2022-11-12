01e8d6202a5585819158afef7fb9d373ec0133ca
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 12:19:11 +0100

    created navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Navbar = () => {
+  return (
+    <section className={style.Navbar}>
+      <div>Navbar</div>
+    </section>
+  );
+};
+
+// mastery by robert hilm
+export default Navbar;
