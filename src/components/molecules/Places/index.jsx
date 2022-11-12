08e32ff820f3ca885f0a26984b9fa40b850d041b
Udoka <udochukwukaonyela@gmail.com>
2022-11-12 17:52:47 +0100

    adding poilerplate

--- src/components/molecules/Places/index.jsx
+++ src/components/molecules/Places/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { PlacesCard } from '../../atoms';
+
 import style from './index.module.scss';
 
 const Places = () => {
   return (
     <section className={style.Places}>
       <h2>Inspiration for your next adventure</h2>
+      <PlacesCard />
     </section>
   );
 };
