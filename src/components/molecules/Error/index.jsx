596412e63d1db985a87ceb63c63794426f610a5a
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 14:27:31 +0100

    styling

--- src/components/molecules/Error/index.jsx
+++ src/components/molecules/Error/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { Err } from '../../../assets';
+
 import style from './index.module.scss';
 // eslint-disable-next-line
 const Error = () => {
   return (
     <section className={style.Error}>
-      <div>Error</div>
+      <div className={style.Error__container}>
+        <Err />
+        <p>Error Connecting to account</p>
+      </div>
     </section>
   );
 };
