9d1070feb97770cf1c10b94c45c1ed3e09b8d0da
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 13:15:28 +0100

    added prop types

--- src/components/atoms/Loader/index.jsx
+++ src/components/atoms/Loader/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
+import { Loading } from '../../../assets';
+
 import style from './index.module.scss';
 
-const Loader = () => {
+const Loader = ({ show }) => {
   return (
-    <section className={style.Loader}>
-      <Loading
+    <section className={`${style.Loader} ${show ? style.show : style.hide}`}>
+      <Loading className={style.Loader__icon} />
     </section>
   );
+};
+
+Loader.propTypes = {
+  show: PropTypes.bool,
 };
 
 export default Loader;
