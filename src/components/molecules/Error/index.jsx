41de3b83f7d21cfd45b9ad1dd12536704757207c
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 14:19:03 +0100

    created erro component

---
+++ src/components/molecules/Error/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Error = () => {
+  return (
+    <section className={style.Error}>
+      <div>Error</div>
+    </section>
+  );
+};
+
+export default Error;
