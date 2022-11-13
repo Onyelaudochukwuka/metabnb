844aed91fb7d1e3f5c103805a639f19bf62c543e
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 13:17:34 +0100

    drilled is loading props

--- src/components/molecules/PopUp/index.jsx
+++ src/components/molecules/PopUp/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -8,9 +8,10 @@ import { Loader, Wallet } from '../../atoms';
 import style from './index.module.scss';
 
 const PopUp = ({ popUp, toggle, setAccount }) => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <section className={`${style.PopUp} ${popUp ? style.show : style.hide}`}>
-      <Loader />
+      <Loader show={isLoading} />
       <div className={style.PopUp__container}>
         <header>
           <h3>Connect Wallet</h3>
@@ -19,8 +20,8 @@ const PopUp = ({ popUp, toggle, setAccount }) => {
         <div className={style.PopUp__container__wallets}>
           <p>Choose your preferred wallet:</p>
           <div className={style.PopUp__container__wallets__links}>
-            <Wallet wallet="Metamask" setAccount={setAccount} />
-            <Wallet wallet="WalletConnect" setAccount={setAccount} />
+            <Wallet wallet="Metamask" setAccount={setAccount} setLoading={(arg) => setIsLoading(arg)} />
+            <Wallet wallet="WalletConnect" setAccount={setAccount} setLoading={(arg) => setIsLoading(arg)} />
           </div>
         </div>
       </div>
