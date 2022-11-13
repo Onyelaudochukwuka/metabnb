afdae24e45b963e27c7d3bfdfcfeab696f28f6f6
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 14:29:19 +0100

    exported error component

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,3 +2,4 @@ export { default as NavLink } from './NavLink';
 export { default as PlacesCard } from './PlacesCard';
 export { default as Wallet } from './Wallet';
 export { default as Loader } from './Loader';
+export { default as Error } from './Error';
