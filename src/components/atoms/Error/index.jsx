5bbb9a3382549805873dfc5bbfefd2e9ea0a5a1c
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 14:49:11 +0100

    created error popUp

--- src/components/atoms/Error/index.jsx
+++ src/components/atoms/Error/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { Err } from '../../../assets';
 
 import style from './index.module.scss';
 // eslint-disable-next-line
-const Error = () => {
+const Error = ({ error }) => {
   return (
-    <section className={style.Error}>
+    <section className={`${style.Error}  ${error ? style.show : style.hide}`}>
       <div className={style.Error__container}>
-        <Err />
+        <Err className={style.Error__container__icon} />
         <p>Error Connecting to account</p>
       </div>
     </section>
   );
+};
+Error.propTypes = {
+  error: PropTypes.bool,
 };
 
 export default Error;
