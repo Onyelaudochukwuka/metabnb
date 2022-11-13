c09c9f41196c2221d1610ca1261e4c38998a17b5
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 12:19:14 +0100

    created loading component

---
+++ src/components/atoms/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './index.module.scss';
+// eslint-disable-next-line
+const Loading = () => {
+  return (
+    <section className={style.Loading}>
+      <div>Loading</div>
+    </section>
+  );
+};
+
+export default Loading;
