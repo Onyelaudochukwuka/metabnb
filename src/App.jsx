a36f5b0287eb444ede5d9d88ab4cd64c1e7c027f
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 15:08:45 +0100

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react';
 
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
-import { Home, PlaceToStay } from './pages';
+import { ComingSoon, Home, PlaceToStay } from './pages';
 
 const App = () => (
   <BrowserRouter>
     <ScrollToTop />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/*" element={<ComingSoon />} />
       <Route path="/place-to-stay" element={<PlaceToStay />} />
     </Routes>
   </BrowserRouter>
