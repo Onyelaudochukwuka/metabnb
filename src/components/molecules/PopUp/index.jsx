480bbf02124c90b57f325ee025ba99338b4ce3a9
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 12:26:42 +0100

    styling

--- src/components/molecules/PopUp/index.jsx
+++ src/components/molecules/PopUp/index.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Close } from '../../../assets';
-import { Wallet } from '../../atoms';
+import { Loading, Wallet } from '../../atoms';
 
 import style from './index.module.scss';
 
 const PopUp = ({ popUp, toggle, setAccount }) => {
   return (
     <section className={`${style.PopUp} ${popUp ? style.show : style.hide}`}>
+      <Loading />
       <div className={style.PopUp__container}>
         <header>
           <h3>Connect Wallet</h3>
