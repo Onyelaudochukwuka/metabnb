4ac391322ac4b047dd20677a37a12e000193cec4
Udoka <udochukwukaonyela@gmail.com>
2022-11-13 12:08:11 +0100

    added feqatures to the layout

--- src/components/molecules/Layout/index.jsx
+++ src/components/molecules/Layout/index.jsx
@@ -6,10 +6,11 @@ import style from './index.module.scss';
 // eslint-disable-next-line
 const Layout = ({ children, ...props }) => {
   const [popUp, setPopUp] = useState(false);
+  const [userAccount, setUserAccount] = useState(false);
   return (
     <section {...props} className={style.Layout}>
-      <Navbar openPopUp={() => setPopUp(true)} />
-      <PopUp {...{ popUp, toggle: () => setPopUp((props) => !props) }} />
+      <Navbar openPopUp={() => setPopUp(true)} account={userAccount} />
+      <PopUp {...{ popUp, toggle: () => setPopUp((props) => !props), setAccount: (arg) => setUserAccount(arg) }} />
       {children}
       <Footer />
     </section>
